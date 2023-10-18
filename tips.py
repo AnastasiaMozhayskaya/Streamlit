dc3d201bb8b69cbdb12f157fff616eb1ebb7ef3f
Анастасия <Anastasia@MacBook-Pro-Anastasia.local>
2023-10-18 15:53:53 +0300

    stream lit

--- tips.py
+++ tips.py
@@ -8,9 +8,6 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
 @st.cache_data
 def load_data():
     """ Utility function for loading the autompg dataset as a dataframe."""
